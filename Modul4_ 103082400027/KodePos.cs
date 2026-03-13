7e1a9338b63dba0bfea8b35cc5ef111a7fb210e0
SiRadit <dharmawanraditya123@gmail.com>
2026-03-13 21:24:18 +0700

    First commit

---
+++ Modul4_ 103082400027/KodePos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4__103082400027
+{
+    internal class KodePos
+    {
+        public string GetKodePos(string kelurahan)
+        {
+            // Menggunakan array paralel sebagai pengganti hashing
+            string[] dataKelurahan = { "Batununggal", "Kujangsari", "Mengger", "Wates", "Cijaura", "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer", "Samoja" };
+            string[] dataKodePos = { "40266", "40287", "40267", "40256", "40287", "40286", "40286", "40286", "40272", "40274", "40273" };
+
+            for (int i = 0; i < dataKelurahan.Length; i++)
+            {
+                if (dataKelurahan[i] == kelurahan)
+                {
+                    return dataKodePos[i];
+                }
+            }
+            return "Kode Pos tidak ditemukan";
+        }
+    }
+}
